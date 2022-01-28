30573408e46fd68405ac1630b607a1724e1fdc7b
Gabriel Lopes <gabrielrrlopes@gmail.com>
2022-01-28 01:29:32 -0300

    Removed unuseless component counter

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import streamReducer from "../features/stream/streamSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     stream: streamReducer,
   },
 });
